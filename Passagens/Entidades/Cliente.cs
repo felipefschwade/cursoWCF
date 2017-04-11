699db42e25ac7aec942a525eaea97104432840ce
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-10 22:35:17 -0400

    Criando a classe clienteService e adicionando as DataNotations para a classe Cliente

--- Passagens/Entidades/Cliente.cs
+++ Passagens/Entidades/Cliente.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Passagens.Entidades
 {
+    [DataContractFormat]
     class Cliente
     {
+        [DataMember]
         public string Cpf { get; set; }
+        [DataMember]
         public string Nome { get; set; }
     }
 }
---
+++ Passagens/Services/ClienteService.cs
@@ -0,0 +1,26 @@
+﻿using Passagens.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Passagens.Entidades;
+using Passagens.DAO;
+
+namespace Passagens.Services
+{
+    class ClienteService : IClienteService
+    {
+        public void Add(Cliente cliente)
+        {
+            var dao = new ClienteDAO();
+            dao.Add(cliente);
+        }
+        public Cliente Buscar(string nome)
+        {
+            var dao = new ClienteDAO();
+            return dao.Buscar(nome);
+        }
+    }
+}
