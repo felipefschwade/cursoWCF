19844c2ea2923bc91c9daaca7f9ba8a432967303
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-10 22:26:37 -0400

    Criando a interface para gerenciar os serviços de clientes.

---
+++ Passagens/Interfaces/IClienteService.cs
@@ -0,0 +1,15 @@
+﻿using Passagens.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens.Interfaces
+{
+    interface IClienteService
+    {
+        Cliente Buscar(string nome);
+        void Add(Cliente cliente);
+    }
+}
