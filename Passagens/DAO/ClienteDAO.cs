d01fcb5981d632186f8a35200db7f23cd113e6cc
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-10 22:22:04 -0400

    Criando a entidade Cliente e seu respectivo DAO

---
+++ Passagens/DAO/ClienteDAO.cs
@@ -0,0 +1,25 @@
+﻿using Passagens.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passagens.DAO
+{
+    class ClienteDAO
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public void Add(Cliente c)
+        {
+            ClienteDAO.clientes.Add(c);
+        }
+
+        public Cliente Buscar(string nome)
+        {
+            var resultado = ClienteDAO.clientes.Where(c => c.Nome.Equals(nome)).FirstOrDefault();
+            return (Cliente)resultado;
+        }
+    }
+}
