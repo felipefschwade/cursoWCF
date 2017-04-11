599c3fef615f7b8f3e30b8b01ac6229cb8a0453a
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-10 22:27:55 -0400

    Adicionando as notações do WCF

--- Passagens/Interfaces/IClienteService.cs
+++ Passagens/Interfaces/IClienteService.cs
@@ -2,14 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Passagens.Interfaces
 {
+    [ServiceContract]
     interface IClienteService
     {
+        [OperationContract]
         Cliente Buscar(string nome);
+        [OperationContract]
         void Add(Cliente cliente);
     }
 }
