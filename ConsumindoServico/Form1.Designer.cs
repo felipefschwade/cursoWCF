c1d94d71ad3d503f4e0a72ee4f46fdbb75b41286
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-04-17 21:35:11 -0400

    Adicionando as lógicas de buscare salvar um cliente e utilzando os métodos do serviço.

--- ConsumindoServico/Form1.Designer.cs
+++ ConsumindoServico/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.btnBuscar.TabIndex = 0;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // btnSalvar
             // 
--- ConsumindoServico/Form1.cs
+++ ConsumindoServico/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ConsumindoServico.PassagensService;
 
 namespace ConsumindoServico
 {
@@ -24,7 +25,41 @@ namespace ConsumindoServico
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var servico = new ClienteServiceClient();
+                var nome = txtNome.Text;
+                var cpf = txtCpf.Text;
+                var cliente = new Cliente()
+                {
+                    Nome = nome,
+                    Cpf = cpf
+                };
+                servico.Add(cliente);
+                MessageBox.Show("Cliente Salvo com Sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possivel salvar o cliente");
+                throw;
+            }
+            
+        }
 
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            var nome = txtNome.Text;
+            try
+            {
+                var service = new ClienteServiceClient();
+                var cliente = service.Buscar(nome);
+                txtCpf.Text = cliente.Cpf;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocorreu um erro ao buscar o cliente");
+                throw;
+            }
         }
     }
 }
